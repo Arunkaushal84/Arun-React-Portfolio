7f947a63624b36c27d52a4707cdaab69a573f425
Arun Kaushal <arunkaushal84@gmail.com>
2023-08-15 01:22:37 -0400

    updated files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 
 // import PortfolioContainer from "./Components/PortfolioContainer";
 
-import Header from '.src/Components/Pages/Header';
+import Header from './components/Header';
 // const App = () => <PortfolioContainer />;
 const App = () => <Header />;
 
